44fc9a1bca5a18aea66015b2ee04659213df6511
RO03M <romera.gv.s@gmail.com>
2024-10-13 14:56:56 -0300

    feat: new directory with context menu

--- src/@core/components/os/desktop/desktop-context.tsx
+++ src/@core/components/os/desktop/desktop-context.tsx
@@ -1,9 +1,21 @@
 import { useCallback } from "preact/compat";
 import { ContextMenu } from "../context-menu/context-menu";
+import { filesystem } from "../../../../app";
+import { safe } from "../../../utils/safe";
 
 export function DesktopContext() {
 	const onShortcut = useCallback((event: KeyboardEvent) => {
 		console.log(event);
+	}, []);
+
+	const newDirectory = useCallback((index = 0) => {
+		const response = safe(() =>
+			filesystem.mkdir(`/home/romera/desktop/new directory(${index})`)
+		);
+
+		if (response.error !== null) {
+			newDirectory(index + 1);
+		}
 	}, []);
 
 	return (
@@ -11,8 +23,15 @@ export function DesktopContext() {
 			<li>
 				<span>New</span>
 				<ul>
-					<li onClick={() => console.log("new directory")} onKeyDown={onShortcut}>Directory</li>
-					<li onClick={() => console.log("new file")} onKeyDown={onShortcut}>File</li>
+					<li
+						onClick={() => newDirectory()}
+						onKeyDown={onShortcut}
+					>
+						Directory
+					</li>
+					<li onClick={() => console.log("new file")} onKeyDown={onShortcut}>
+						File
+					</li>
 				</ul>
 			</li>
 		</ContextMenu>
